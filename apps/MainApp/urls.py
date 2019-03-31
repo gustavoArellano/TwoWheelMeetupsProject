e4306b085011e4d840a85e396ce32925389dfe2a
Gustavo Arellano <gustavo813arellano@gmail.com>
2019-03-30 21:14:45 -0700

    loginreg started

--- apps/MainApp/urls.py
+++ apps/MainApp/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index, name="index"),
+    url(r'^register', views.RegistrationPage, name="register"),
+    url(r'login', views.LoginPage, name="login")
 ]
--- apps/MainApp/views.py
+++ apps/MainApp/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponseRedirect
 
 def index(request):
     return render(request, "MainApp/index.html")
+
+def RegistrationPage(request):
+    return render(request, "MainApp/register.html")
+
+def LoginPage(request):
+    return render(request, "MainApp/login.html")

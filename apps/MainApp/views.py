c324d7f6b3044a4f5e3f28d2c9372d933e25b5a2
Gustavo Arellano <gustavo813arellano@gmail.com>
2019-03-31 20:12:39 -0700

    made model migrations

--- apps/MainApp/views.py
+++ apps/MainApp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect, redirect
 from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.contrib.messages import get_messages 
+from .models import User
+import datetime
+from time import strftime
+import bcrypt
 
 def index(request):
     return render(request, "MainApp/index.html")

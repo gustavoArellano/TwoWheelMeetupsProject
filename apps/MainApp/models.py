f468563c7e1d9c5320bbb1c2305db6f2e2ad50ab
Gustavo Arellano <gustavo813arellano@gmail.com>
2019-03-31 22:45:30 -0700

    Finished RegValidations And Started Other Models

--- apps/MainApp/models.py
+++ apps/MainApp/models.py
@@ -6,37 +6,86 @@ from django import forms
 import re, bcrypt
 
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-    
+
+class UserManager(models.Manager):
+    def RegValidation(self, postData):
+        errors = {}
+
+        if len(postData['FirstName']) < 1:
+            errors['FirstName'] = "FIRST NAME cannot be BLANK!"
+        elif len(postData['FirstName']) < 2:
+            errors['FirstName'] = "FIRST NAME must contain at least 2 letters MINIMUM!"
+        elif not postData['FirstName'].isalpha():
+            errors['FirstName'] = "FIRST NAME must contain letter's ONLY!"
+
+        if len(postData['LastName']) < 1:
+            errors['LastName'] = "LAST NAME cannot be blank!"
+        elif len(postData['LastName']) < 2:
+            errors['LastName'] = "LAST NAME must contain at least 2 letters MINIMUM!" 
+        elif not postData['Last Name'].isalpha():
+            errors['LastName'] = "LAST NAME must contain letter's ONLY"
+
+        if User.objects.filter(email = postData['email']):
+            errors['EmailExists'] = "An account has already been created with this EMAIL!"
+        if EMAIL_REGEX.match(postData['Email']) == None:
+            errors['EmailFormat'] = "Invalid EMAIL FORMAT!"
+        elif len(postData['Email']) < 1:
+            errors['Email'] = "EMAIL cannot be BLANK!"
+
+        if len(postData['State']) < 1:
+            errors['State'] = "You must select a STATE!"
+
+        if len(postData['City']) < 1:
+            errors['City'] = "You must select a City!"
+
+        if len(postData['ZipCode']) < 1:
+            errors['ZipCode'] = "You must enter your ZIP CODE!"
+        if len(postData['ZipCode']) > 5:
+            errors['ZipCode'] = "You must enter a valid ZIP CODE!"
+        elif not postData['ZipCode'].isinteger():
+            errors['ZipCode'] = "ZIP CODE must contain numbers ONLY!"
+
+        if len(postData['Password']) < 1:
+            errors['Password'] = "PASSWORD cannot be BLANK!"
+        elif len(postData['Password']) < 6:
+            errors['PasswordLength'] = "PASSWORD must be at least 6 characters MINIMUM!"
+
+        if postData['Password'] != postData['ConfirmPassword']:
+            errors['ConfirmPassword'] = "PASSWORDS do not MATCH!"
+
+        return errors
+
 class User(models.Model): 
     FirstName = models.CharField(max_length = 20) 
     LastName = models.CharField(max_length = 20)
     Email = models.CharField(max_length = 255)
+    State = models.CharField(max_length = 2)
     City = models.CharField(max_length = 20)
-    State = models.CharField(max_length = 3)
-    ZipCode = models.CharField(max_length = 6)
+    ZipCode = models.CharField(max_length = 5)
+    Password = models.CharField(max_length = 255, default = True)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
-    # objects = UserManager()
+    objects = UserManager()
 
 class Event(models.Model):
     Title = models.CharField(max_length = 255)
     Description = models.CharField(max_length = 255)
     EventDate = models.CharField(max_length = 255)
-    Address = models.CharField(max_length = 255)
+    Address = models.CharField(max_length = 255, unique = True)
     City = models.CharField(max_length = 20)
     State = models.CharField(max_length = 3)
     ZipCode = models.CharField(max_length = 6)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
-    CreatedByUser = models.ForeignKey(User, related_name = "CreatedByUser")
-    # objects = UserManager()
+    EventByUser = models.ForeignKey(User, related_name = "EventsByUser")
+    # objects = EventManager()
 
 class EventComment(models.Model):
     Comment = models.CharField(max_length = 255)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
     Event = models.ForeignKey(Event, related_name = "Comments")
-    PostedByUser = models.ForeignKey(User, related_name = "PostedByUser")
+    CommentByUser = models.ForeignKey(User, related_name = "CommentsByUser")
 
 class EventAttendant(models.Model):
     created_at = models.DateTimeField(auto_now_add = True)

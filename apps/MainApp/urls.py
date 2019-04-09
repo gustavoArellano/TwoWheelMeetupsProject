cefb6836b4e8b41cb6735b80ea31d07594947099
Gustavo Arellano <gustavo813arellano@gmail.com>
2019-04-08 20:31:05 -0700

    Registration Functional

--- apps/MainApp/urls.py
+++ apps/MainApp/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.Index, name="Index"),
-    url(r'^Register', views.RegistrationPage, name="Register"),
-    url(r'^Login', views.LoginPage, name="Login"),
+    url(r'^Register$', views.RegistrationPage, name="Register"),
+    url(r'^Login$', views.LoginPage, name="Login"),
     url(r'^RegistrationProcess$', views.RegistrationProcess, name="RegistrationProcess"),
     url(r'^Home$', views.Home, name="Home")
 ]

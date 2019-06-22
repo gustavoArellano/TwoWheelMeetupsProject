f65fb596de2b26337929c266395642bcc19383ee
Gustavo Arellano <gustavo813arellano@gmail.com>
2019-06-22 12:52:04 -0700

    map api complete

---
+++ apps/MainApp/static/MainApp/js/googleMaps.js
@@ -0,0 +1,57 @@
+var geocoder;
+var map;
+function initialize() {
+    geocoder = new google.maps.Geocoder();
+
+    var latlng = new google.maps.LatLng(36.7783, 119.4179);
+
+    var mapOptions = {
+        zoom: 13,
+        center: latlng,
+        scaleControl: false,
+        fullscreenControl: false,
+        zoomControl: true,
+        mapTypeControl: false,
+        streetViewControl: false
+        }
+    
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
+    
+function codeAddress() {
+    var address = document.getElementById('address').value;
+
+    geocoder.geocode( { 'address': address}, function(results, status) {
+
+        if (status == 'OK') {
+            map.setCenter(results[0].geometry.location);
+
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location,
+                animation: google.maps.Animation.DROP
+                });
+            
+            var infowindow = new google.maps.InfoWindow({
+                content:"<div class='infoWindow'><p><strong>We Meet Here!!!</strong></p>***SEE DESCRIPTION FOR DETAILS***</div>"
+                });
+
+            google.maps.event.addListener(marker, 'click', function() {
+                infowindow.open(map,marker);
+            });
+
+            marker.addListener('click', toggleBounce);
+            
+            function toggleBounce() {
+                if (marker.getAnimation() !== null) {
+                    marker.setAnimation(null);
+                } else {
+                    marker.setAnimation(google.maps.Animation.BOUNCE);
+                    setTimeout(function(){ marker.setAnimation(null); }, 750);
+                }
+            }
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}

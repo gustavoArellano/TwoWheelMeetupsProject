0fadccdfa3f4dfbbcb9f5a92b096a6416f19dcc2
Gustavo Arellano <gustavo813arellano@gmail.com>
2019-07-07 22:07:14 -0700

    settings update

--- TwoWheelMeetups/settings.py
+++ TwoWheelMeetups/settings.py
@@ -5,17 +5,14 @@ BOOTSTRAP4 = {
     'include_jquery': True,
 }
 
-BASE_DIR = os.path.dirname(
-            os.path.dirname(    
-            os.path.dirname(
-            os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 SECRET_KEY = config('SECRET_KEY')
 API_KEY = config('API')
 
 DEBUG = True
-
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
@@ -59,6 +56,29 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'TwoWheelMeetups.wsgi.application'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -69,11 +89,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+
+
 STATIC_URL = config('myStaticURL')
 STATIC_ROOT = config('myStaticRoot')
 
 MEDIA_ROOT = config('myMediaRoot')
 MEDIA_URL = config('myMediaURL')
-
-
-
